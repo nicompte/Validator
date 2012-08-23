27ff0a64a2958912601772c1838796d0d882913b
Nicolas Barbotte <n.barbotte@gmail.com>
2012-08-23 22:14:26 +0200

    clean up

--- tests/test-validator.js
+++ tests/test-validator.js
@@ -3,42 +3,42 @@ var should = typeof chai !== 'undefined' ? chai.should() : require("chai").shoul
 
 describe('Rules', function() {
 	describe('required', function() {
-		it('should fail when null', function() {
+		it('should fail for null', function() {
 			Validator.rules.required(null).should.be.false;
 		});
-		it('should fail when undefined', function() {
+		it('should fail for undefined', function() {
 			Validator.rules.required(undefined).should.be.false;
 		});
-		it('should fail when ""', function() {
+		it('should fail for ""', function() {
 			Validator.rules.required("").should.be.false;
 		});
-		it('should fail when " "', function() {
+		it('should fail for " "', function() {
 			Validator.rules.required(" ").should.be.false;
 		});
-		it('should fail when "	"', function() {
+		it('should fail for "	"', function() {
 			Validator.rules.required(" 	 ").should.be.false;
 		});
-		it('should succeed when 0', function() {
+		it('should succeed for 0', function() {
 			Validator.rules.required(0).should.be.true;
 		});
-		it('should succeed when "a"', function() {
+		it('should succeed for "a"', function() {
 			Validator.rules.required("a").should.be.true;
 		});
-		it('should succeed when {}', function() {
+		it('should succeed for {}', function() {
 			Validator.rules.required({}).should.be.true;
 		});
-		it('should succeed when []', function() {
+		it('should succeed for []', function() {
 			Validator.rules.required([]).should.be.true;
 		});
 	});
 	describe('maxLength', function() {
-		it('should fail when "1234"', function() {
+		it('should fail for "1234"', function() {
 			Validator.rules.maxLength("1234", 3).should.be.false;
 		});
-		it('should fail when "    "', function() {
+		it('should fail for "    "', function() {
 			Validator.rules.maxLength("    ", 3).should.be.false;
 		});
-		it('should succeed when "123"', function() {
+		it('should succeed for "123"', function() {
 			Validator.rules.maxLength("123", 3).should.be.true;
 		});
 		/*
@@ -48,13 +48,13 @@ describe('Rules', function() {
 		*/
 	});
 	describe('minLength', function() {
-		it('should fail when "12"', function() {
+		it('should fail for "12"', function() {
 			Validator.rules.minLength("12", 3).should.be.false;
 		});
-		it('should fail when "  "', function() {
+		it('should fail for "  "', function() {
 			Validator.rules.minLength("  ", 3).should.be.false;
 		});
-		it('should succeed when "123"', function() {
+		it('should succeed for "123"', function() {
 			Validator.rules.minLength("123", 3).should.be.true;
 		});
 		/*
@@ -64,19 +64,19 @@ describe('Rules', function() {
 		*/
 	});
 	describe('rangeLength', function() {
-		it('[3, 5] should fail when "12"', function() {
+		it('[3, 5] should fail for "12"', function() {
 			Validator.rules.rangeLength("12", [3, 5]).should.be.false;
 		});
-		it('[3, 5] should fail when "123456"', function() {
+		it('[3, 5] should fail for "123456"', function() {
 			Validator.rules.rangeLength("123456", [3, 5]).should.be.false;
 		});
-		it('[3, 5] should succeed when "12345"', function() {
+		it('[3, 5] should succeed for "12345"', function() {
 			Validator.rules.rangeLength("12345", [3, 5]).should.be.true;
 		});
-		it('[3, 5] should succeed when "123"', function() {
+		it('[3, 5] should succeed for "123"', function() {
 			Validator.rules.rangeLength("123", [3, 5]).should.be.true;
 		});
-		it('[3, 5] should succeed when "1234"', function() {
+		it('[3, 5] should succeed for "1234"', function() {
 			Validator.rules.rangeLength("1234", [3, 5]).should.be.true;
 		});
 		/*
@@ -86,36 +86,36 @@ describe('Rules', function() {
 		*/
 	});
 	describe('digits', function() {
-		it('should fail when "e12"', function() {
+		it('should fail for "e12"', function() {
 			Validator.rules.digits("e12").should.be.false;
 		});
-		it('should fail when " 12"', function() {
+		it('should fail for " 12"', function() {
 			Validator.rules.digits(" 12").should.be.false;
 		});
-		it('should succeed when "12"', function() {
+		it('should succeed for "12"', function() {
 			Validator.rules.digits("12").should.be.true;
 		});
-		it('should succeed when 12', function() {
+		it('should succeed for 12', function() {
 			Validator.rules.digits(12).should.be.true;
 		});
-		it('should fail when null', function() {
+		it('should fail for null', function() {
 			Validator.rules.digits(null).should.be.false;
 		});
 	});
 	describe('in', function() {
-		it('["A"] should fail when "a"', function() {
+		it('["A"] should fail for "a"', function() {
 			Validator.rules["in"]("a", ["A"]).should.be.false;
 		});
-		it('[] should fail when "a"', function() {
+		it('[] should fail for "a"', function() {
 			Validator.rules["in"]("a", []).should.be.false;
 		});
-		it('[1, "b"] should fail when "1"', function() {
+		it('[1, "b"] should fail for "1"', function() {
 			Validator.rules["in"]("1", [1, "b"]).should.be.false;
 		});
-		it('["a", "b"] should succeed when "a"', function() {
+		it('["a", "b"] should succeed for "a"', function() {
 			Validator.rules["in"]("a", ["a", "b"]).should.be.true;
 		});
-		it('[1 : "b"] should succeed when 1', function() {
+		it('[1 : "b"] should succeed for 1', function() {
 			Validator.rules["in"](1, [1, "b"]).should.be.true;
 		});
 		/*
@@ -125,19 +125,19 @@ describe('Rules', function() {
 		*/
 	});
 	describe('notIn', function() {
-		it('["A"] should succeed when "a"', function() {
+		it('["A"] should succeed for "a"', function() {
 			Validator.rules.notIn("a", ["A"]).should.be.true;
 		});
-		it('[] should succeed when "a"', function() {
+		it('[] should succeed for "a"', function() {
 			Validator.rules.notIn("a", []).should.be.true;
 		});
-		it('[1, "b"] should succeed when "1"', function() {
+		it('[1, "b"] should succeed for "1"', function() {
 			Validator.rules.notIn("1", [1, "b"]).should.be.true;
 		});
-		it('["a", "b"] should fail when "a"', function() {
+		it('["a", "b"] should fail for "a"', function() {
 			Validator.rules.notIn("a", ["a", "b"]).should.be.false;
 		});
-		it('[1, "b"] should fail when 1', function() {
+		it('[1, "b"] should fail for 1', function() {
 			Validator.rules.notIn(1, [1, "b"]).should.be.false;
 		});
 		/*
@@ -147,143 +147,143 @@ describe('Rules', function() {
 		*/
 	});
 	describe('number', function() {
-		it('should fail when "a"', function() {
+		it('should fail for "a"', function() {
 			Validator.rules.number("a").should.be.false;
 		});
-		it('should fail when "1"', function() {
+		it('should fail for "1"', function() {
 			Validator.rules.number("1").should.be.false;
 		});
-		it('should succeed when 1', function() {
+		it('should succeed for 1', function() {
 			Validator.rules.number(1).should.be.true;
 		});
-		it('should fail when null', function() {
+		it('should fail for null', function() {
 			Validator.rules.number(null).should.be.false;
 		});
 	});
 	describe('min', function() {
-		it('1 should fail when "a"', function() {
+		it('1 should fail for "a"', function() {
 			Validator.rules.min("a", 1).should.be.false;
 		});
-		it('1 should fail when 0', function() {
+		it('1 should fail for 0', function() {
 			Validator.rules.min(0, 1).should.be.false;
 		});
-		it('1 should succeed when 1', function() {
+		it('1 should succeed for 1', function() {
 			Validator.rules.min(1, 1).should.be.true;
 		});
-		it('1 should succeed when 2', function() {
+		it('1 should succeed for 2', function() {
 			Validator.rules.min(2, 1).should.be.true;
 		});
-		it('1should fail when null', function() {
+		it('1should fail for null', function() {
 			Validator.rules.min(null, 1).should.be.false;
 		});
 	});
 	describe('max', function() {
-		it('1 should fail when "a"', function() {
+		it('1 should fail for "a"', function() {
 			Validator.rules.max("a", 1).should.be.false;
 		});
-		it('1 should succeed when 0', function() {
+		it('1 should succeed for 0', function() {
 			Validator.rules.max(0, 1).should.be.true;
 		});
-		it('1 should succeed when 1', function() {
+		it('1 should succeed for 1', function() {
 			Validator.rules.max(1, 1).should.be.true;
 		});
-		it('1 should fail when 2', function() {
+		it('1 should fail for 2', function() {
 			Validator.rules.max(2, 1).should.be.false;
 		});
-		it('1 should fail when null', function() {
+		it('1 should fail for null', function() {
 			Validator.rules.max(null, 1).should.be.false;
 		});
 	});
 	describe('smallerThan', function() {
-		it('1 should fail when "a"', function() {
+		it('1 should fail for "a"', function() {
 			Validator.rules.smallerThan("a", 1).should.be.false;
 		});
-		it('1 should succeed when 0', function() {
+		it('1 should succeed for 0', function() {
 			Validator.rules.smallerThan(0, 1).should.be.true;
 		});
-		it('1 should fail when 1', function() {
+		it('1 should fail for 1', function() {
 			Validator.rules.smallerThan(1, 1).should.be.false;
 		});
-		it('1 should fail when 2', function() {
+		it('1 should fail for 2', function() {
 			Validator.rules.smallerThan(2, 1).should.be.false;
 		});
-		it('1 should fail when null', function() {
+		it('1 should fail for null', function() {
 			Validator.rules.smallerThan(null, 1).should.be.false;
 		});
 	});
 	describe('biggerThan', function() {
-		it('1 should fail when "a"', function() {
+		it('1 should fail for "a"', function() {
 			Validator.rules.biggerThan("a", 1).should.be.false;
 		});
-		it('1 should fail when 0', function() {
+		it('1 should fail for 0', function() {
 			Validator.rules.biggerThan(0, 1).should.be.false;
 		});
-		it('1 should fail when 1', function() {
+		it('1 should fail for 1', function() {
 			Validator.rules.biggerThan(1, 1).should.be.false;
 		});
-		it('1 should succeed when 2', function() {
+		it('1 should succeed for 2', function() {
 			Validator.rules.biggerThan(2, 1).should.be.true;
 		});
-		it('1 should fail when null', function() {
+		it('1 should fail for null', function() {
 			Validator.rules.biggerThan(null, 1).should.be.false;
 		});
 	});
 	describe('range', function() {
-		it('[3, 5] should fail when "a"', function() {
+		it('[3, 5] should fail for "a"', function() {
 			Validator.rules.range("a", [3, 5]).should.be.false;
 		});
-		it('[3, 5] should fail when 0', function() {
+		it('[3, 5] should fail for 0', function() {
 			Validator.rules.range(0, [3, 5]).should.be.false;
 		});
-		it('[3, 5] should succeed when 3', function() {
+		it('[3, 5] should succeed for 3', function() {
 			Validator.rules.range(3, [3, 5]).should.be.true;
 		});
-		it('[3, 5] should succeed when 5', function() {
+		it('[3, 5] should succeed for 5', function() {
 			Validator.rules.range(5, [3, 5]).should.be.true;
 		});
-		it('[3, 5] should fail when null', function() {
+		it('[3, 5] should fail for null', function() {
 			Validator.rules.range(null, [3, 5]).should.be.false;
 		});
 	});
 	describe('is', function() {
-		it('1 should fail when "a"', function() {
+		it('1 should fail for "a"', function() {
 			Validator.rules.is("a", 1).should.be.false;
 		});
-		it('1 should fail when "1"', function() {
+		it('1 should fail for "1"', function() {
 			Validator.rules.is("1", 1).should.be.false;
 		});
-		it('1 should succeed when 1', function() {
+		it('1 should succeed for 1', function() {
 			Validator.rules.is(1, 1).should.be.true;
 		});
-		it('1 should fail when null', function() {
+		it('1 should fail for null', function() {
 			Validator.rules.is(null, 1).should.be.false;
 		});
 	});
 	describe('isnt', function() {
-		it('1 should succeed when "a"', function() {
+		it('1 should succeed for "a"', function() {
 			Validator.rules.isnt("a", 1).should.be.true;
 		});
-		it('1 should succeed when "1"', function() {
+		it('1 should succeed for "1"', function() {
 			Validator.rules.isnt("1", 1).should.be.true;
 		});
-		it('1 should fail when 1', function() {
+		it('1 should fail for 1', function() {
 			Validator.rules.isnt(1, 1).should.be.false;
 		});
-		it('1 should succeed when null', function() {
+		it('1 should succeed for null', function() {
 			Validator.rules.isnt(null, 1).should.be.true;
 		});
 	});
 	describe('pattern', function() {
-		it('/^[a-z]$/ should fail when "A"', function() {
+		it('/^[a-z]$/ should fail for "A"', function() {
 			Validator.rules.pattern("A", /^[a-z]$/).should.be.false;
 		});
-		it('/^[a-z]$/ should fail when "1"', function() {
+		it('/^[a-z]$/ should fail for "1"', function() {
 			Validator.rules.pattern("1", /^[a-z]$/).should.be.false;
 		});
-		it('/^[a-z]$/ should succeed when "a"', function() {
+		it('/^[a-z]$/ should succeed for "a"', function() {
 			Validator.rules.pattern("a", /^[a-z]$/).should.be.true;
 		});
-		it('/^[a-z]$/ should fail when null', function() {
+		it('/^[a-z]$/ should fail for null', function() {
 			Validator.rules.pattern(null, /^[a-z]$/).should.be.false;
 		});
 	});
